031aa75323e1a2a43b310c1c6a9a23ad074a85fe
Wilmer Ansgariusson <wansgariusson@gmail.com>
2019-12-14 18:33:33 +0100

    Day 14: Progress, incorrect weighting

--- 14/14.py
+++ 14/14.py
@@ -34,12 +34,16 @@ def is_orereaction(reaction):
 def find_reaction(reactions, resulttype):
     return next(r for r in reactions if r.result.type == resulttype)
 
-def totalore(reaction, amount):
-    if is_orereaction(reaction):
-        ore = next(c for c in reaction.components if c.type == "ORE")
-        return math.ceil(amount / reaction.result.amount)*ore.amount
-    else:
-        return sum([totalore(find_reaction(reactions, c.type), amount) for c in reaction.components])
+def print_fuel_components(fuelreaction):
+    nums = {}
+    for comp in fuelreaction.components:
+        ty = comp.type
+        am = comp.amount
+        if ty in nums:
+            nums[ty] += am
+        else:
+            nums[ty] = am
+    print(nums)
 
 reactions = set()
 
@@ -55,7 +59,58 @@ for line in [x.strip() for  x in sys.stdin.readlines()]:
 
 fuelreaction = next(r for r in reactions if r.result.type == "FUEL")
 orereactions = set([r for r in reactions if is_orereaction(r)])
+oreresulttypes = [o.result.type for o in orereactions]
 print(fuelreaction)
-print(orereactions)
+#print(orereactions)
+#print(oreresulttypes)
 
-print(totalore(fuelreaction, 1))
+def weight(component, reactions):
+    if component.type == "ORE":
+        return 0 # 0 distance from ore
+    else:
+        return min([weight(c, reactions) for c in find_reaction(reactions, component.type).components]) + 1
+
+# define weights
+weights = {} # type -> weight
+for reaction in reactions:
+    for comp in reaction.components:
+        if comp.type not in weights:
+            weights[comp.type] = weight(comp, reactions)
+
+print("weights : " + str(weights))
+
+def max_weighted(components, weights):
+    maxweight = max([weights[c.type] for c in components])
+    return [comp for comp in components if weights[comp.type] == maxweight]
+
+while not all(c.type == "ORE" for c in fuelreaction.components):
+    # change all components to their components if not already of type ore
+    newcomps = []
+    for c in fuelreaction.components:
+        if c in max_weighted(fuelreaction.components, weights):
+            if c.type != "ORE":
+                # change to its components
+                am = c.amount
+                re = find_reaction(reactions, c.type)
+                for c2 in re.components:
+                    c2.amount *= math.ceil(am / re.result.amount)
+                    newcomps.append(c2)
+            else:
+                newcomps.append(c)
+        else:
+            newcomps.append(c)
+    print(newcomps)
+    # fuse identical component types
+    newcomps2 = []
+    for i in range(len(newcomps)):
+        do_add = True
+        for j in range(i+1, len(newcomps)):
+            if newcomps[i].type == newcomps[j].type:
+                newcomps[j].amount += newcomps[i].amount
+                do_add = False
+                break
+        if do_add:
+            newcomps2.append(newcomps[i])
+    fuelreaction.components = newcomps2
+    print(newcomps2)
+    #print_fuel_components(fuelreaction)
